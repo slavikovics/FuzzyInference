1d6d3f7c4fe2d7d2a71fb06386e34613d437fe37
froonn <137333195+froonn@users.noreply.github.com>
2025-09-26 17:00:04 +0300

    fix example_of_using_parser.py

--- example_of_using_parser.py
+++ example_of_using_parser.py
@@ -1,18 +1,15 @@
 #%%
-from parser import FuzzySetParser, ImplicationParser
+from parser import parse_fuzzy_set, parse_fuzzy_implication
 
 list_of_sets = []
 list_of_imps = []
 
-set_parser = FuzzySetParser(list_of_sets)
-imp_parser = ImplicationParser(list_of_imps, list_of_sets)
+list_of_sets.append(parse_fuzzy_set('A = {<a, 1>, <c, 0>, <b, 0.9>}'))
+list_of_sets.append(parse_fuzzy_set('B = {<a, 1>, <c, 0>, <b, 0.9>}'))
+list_of_sets.append(parse_fuzzy_set('C = {<a, 1>, <c, 0>, <b, 0.9>}'))
 
-set_parser.parse('A = {<a, 1>, <c, 0>, <b, 0.9>}')
-set_parser.parse('B = {<a, 1>, <c, 0>, <b, 0.9>}')
-set_parser.parse('C = {<a, 1>, <c, 0>, <b, 0.9>}')
-
-imp_parser.parse('A ~> B')
-imp_parser.parse('B ~> C')
+list_of_imps.append(parse_fuzzy_implication('A ~> B'))
+list_of_imps.append(parse_fuzzy_implication('B ~> C'))
 
 print(list_of_sets)
 print(list_of_imps)
