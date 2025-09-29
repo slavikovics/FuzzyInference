47577d661acc4ddfc6417694139fdb6a67ad6696
RedmiBook <puskomaks557@gmail.com>
2025-09-29 19:11:24 +0300

    add description

--- fuzzy_inference_tests/__init__.py
+++ fuzzy_inference_tests/__init__.py
@@ -1,3 +1,17 @@
+"""
+Выполнили студенты группы 321701:
+- Мотолянец Кирилл Андреевич
+- Пушко Максим Александрович
+- Самович Вячеслав Максимович
+Вариант 4
+
+Главное меню программы
+27.09.2025
+
+Источники:
+- Логические основы интеллектуальных систем. Практикум : учебно - метод. пособие / В. В. Голенков [и др.]. – Минск : БГУИР, 2011. – 70 с. : ил.
+"""
+
 from fuzzy_inference_tests.test_fuzzy_set_parser import FuzzySetParserTests
 from fuzzy_inference_tests.test_implication_parser import FuzzyImplicationParserTests
 from fuzzy_inference_tests.test_inference_input import InferenceInputTests
